eed1f1a33143003497eed5ace291a34782e09397
George Beckstein <donbeckstein@live.com>
2020-06-29 17:32:41 -0400

    Add license and reformat MCP23008 driver to conform to EP standards

--- devices/MCP23008/MCP23008.cpp
+++ devices/MCP23008/MCP23008.cpp
@@ -1,4 +1,29 @@
+/**
+ * ep-oc-mcu
+ * Embedded Planet Open Core for Microcontrollers
+ *
+ * Built with ARM Mbed-OS
+ *
+ * Copyright (c) 2019-2020 Embedded Planet, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 #include "MCP23008.hpp"
+
+#include "mbed_error.h"
 
 namespace {
 const uint8_t MCP23008_ADDRESS  = 0x40;
